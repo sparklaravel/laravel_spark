4e8fdd8d94df243c973590db9e1591ee70e0bb60
Askariot <magicjoke@gmail.com>
2019-02-21 12:40:25 +0200

    21.02 #1

---
+++ database/migrations/2019_02_20_115309_create_polls_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePollsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('poll_status')->nullable();
+            $table->string('poll_name')->nullable();
+            $table->string('poll_type')->nullable();
+            $table->string('poll_question')->nullable();
+            $table->string('poll_answers')->nullable();
+            $table->string('poll_audience')->nullable();
+            $table->string('poll_audience_gender')->nullable();
+            $table->string('poll_audience_age_from')->nullable();
+            $table->string('poll_audience_age_to')->nullable();
+            $table->string('poll_audience_location')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('polls');
+    }
+}
